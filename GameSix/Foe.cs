ca2591ab181cb3dfc0a1564f9caa2692f8246618
Daniel <danielmordelfer@gmail.com>
2023-04-21 13:14:17 +0100

    new methods for damage taken and for name

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -6,7 +6,7 @@ namespace GameSix
         private string name;
         private float health;
         private float shield;
-        
+
         public Foe(string name)
         {
             this.name = name;
@@ -14,7 +14,23 @@ namespace GameSix
             shield = 0;
         }
 
+        public string Getname()
+        {
+            return name;
+        }
+        
+        public void TakeDamage (float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
+
+
     }
-
-
 }
