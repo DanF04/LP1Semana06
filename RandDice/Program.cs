430d7b2069984b0aa216bd8a72ca5ea397c5da06
Daniel <danielmordelfer@gmail.com>
2025-03-28 17:21:08 +0000

    New program that rolls dice

---
+++ RandDice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+ 
+ namespace RandDice
+ {
+     public class Program
+     {
+         private static void Main(string[] args)
+         {
+             if (args.Length != 2)
+             {
+                 Console.WriteLine("Uso: dotnet run --project RandDice -- <n> <s>");
+                 Console.WriteLine("n: número de dados");
+                 Console.WriteLine("s: seed para o gerador aleatório");
+                 return;
+             }
+ 
+
+         }
+     }
+ }
