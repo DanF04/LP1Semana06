551161043a31f60422795640ec7e1c091ab41643
Daniel <danielmordelfer@gmail.com>
2025-03-28 17:09:53 +0000

    Used stopwatch commands to the variables

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -11,6 +11,13 @@
              Console.WriteLine("Hello LP1!");
              Stopwatch crono1 = new Stopwatch();
              Stopwatch crono2 = new Stopwatch();
+             
+             crono1.Start();
+             Thread.Sleep(500);
+             crono2.Start();
+             Thread.Sleep(250);
+             crono1.Stop();
+             crono2.Stop();
          }
      }
  }
