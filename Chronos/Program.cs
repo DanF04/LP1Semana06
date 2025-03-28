8a3cfb71f8e65a29444b2a7713ac120757e20c53
Daniel <danielmordelfer@gmail.com>
2025-03-28 17:08:55 +0000

    New program that uses stopwatch

---
+++ Chronos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+ using System.Diagnostics;
+ using System.Threading;
+ 
+ namespace Chronos
+ {
+     public class Program
+     {
+         private static void Main(string[] args)
+         {
+             Console.WriteLine("Hello LP1!");
+             Stopwatch crono1 = new Stopwatch();
+             Stopwatch crono2 = new Stopwatch();
+         }
+     }
+ }
